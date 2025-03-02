159bab2468f22f18660f9e1dbe444e27a1c5d096
Mohammadali Talaei <altalaie@gmail.com>
2025-03-02 11:03:22 -0500

    added netlify config"

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,12 +5,10 @@ import Header from './Header.jsx';
 const Layout = () => {
   return (
     <div className='w-[100vw] h-[100vh] flex items-center justify-center'>
-        <main className='w-[30rem] max-h-[50rem] overflow-auto shadow-lg rounded-xl'>
-      <Header />
-      <main style={{ padding: '1rem' }}>
+      <main className='w-[100%] lg:w-[30rem] h-[100%] lg:max-h-[50rem] overflow-auto shadow-lg rounded-xl'>
+        <Header />
         <Outlet />
       </main>
-    </main>
     </div>
     
   );
